3eb80d01680e8814ac0e01cf2f0e9d9cca32c83d
Martin-Louis Bright <mlbright@gmail.com>
2010-11-04 09:59:13 -0400

    Adding Joris van Rantwijk's code for maximum weight matching in an arbitrary graph.

--- kuhnMunkres.py
+++ kuhnMunkres.py
@@ -103,7 +103,7 @@ def maxWeightMatching(weights):
   
 #  a small example 
 
-#print maxWeightMatching([[1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]])
+print maxWeightMatching([[1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]])
 
 # read from standard input a line with n
 # then n*n lines with u,v,w[u][v]
