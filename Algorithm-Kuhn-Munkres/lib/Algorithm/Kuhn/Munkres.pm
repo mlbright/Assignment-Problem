ef8a29d7e6da7d2b12f9de0607025859d468816a
Martin-Louis Bright <mlbright@gmail.com>
2010-11-06 23:14:28 -0400

    documentation

--- Algorithm-Kuhn-Munkres/lib/Algorithm/Kuhn/Munkres.pm
+++ Algorithm-Kuhn-Munkres/lib/Algorithm/Kuhn/Munkres.pm
@@ -152,95 +152,66 @@ This document describes Algorithm::Kuhn::Munkres version 0.0.3
     use Algorithm::Kuhn::Munkres;
 
 =for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
+    
+    use Algorithm::Kuhn::Munkres;
+    
+    my @matrix = ([1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]);
+    my ($cost,$mapping) = assign(@matrix);
+ 
   
 =head1 DESCRIPTION
 
 =for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
 
+    Implementation of the Kuhn-Munkres algorithm. This algorithm finds the maximum weight
+    perfect matching in a weighted complete bipartite graph. This problem is known as 
+    the "Assignment Problem".
 
 =head1 INTERFACE 
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+=over 4
 
+=item max_weight_perfect_matching
+
+Determines the maximum weight perfect matching in a weighted complete bipartite graph.
+The single argument is a matrix representing the weights of the edges in the bipartite graph.
+The matrix must be implemented as a list of array objects.
+
+    use Algorithm::Kuhn::Munkres;
+
+    my @matrix = ([1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]);
+    my ($cost,$mapping) = assign(@matrix);
+    
+
+=item assign
+
+Synonym for max_weight_perfect_matching
+
+=back
 
 =head1 DIAGNOSTICS
 
-=for author to fill in:
-    List every single error and warning message that the module can
+    Ideally, I would every single error and warning message that the module can
     generate (even the ones that will "never happen"), with a full
     explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
+    suggested remedies. I'm not that good a person right now.
 
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
 Algorithm::Kuhn::Munkres requires no configuration files or environment variables.
 
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
+List::Util
 
 =head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
 
 None reported.
 
 
 =head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
 
 No bugs have been reported.
 
